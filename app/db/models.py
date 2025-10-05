933447107735e3c7d156eedb91d9b35b0db5cd11
zenvxous <zenvxous@gmail.com>
2025-10-06 01:02:28 +0300

    core models

--- app/db/models.py
+++ app/db/models.py
@@ -1,13 +1,20 @@
-from random import randint
 from uuid import UUID as uuid_default
-
+from enum import Enum
+from sqlalchemy import (
+    UUID,
+    String,
+    Integer,
+    Text,
+    ARRAY,
+)
 from sqlalchemy import (
     Column,
     DateTime,
     func,
 )
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, relationship
 from uuid_utils import uuid7
+from app.utils.common import encrypt_password, decrypt_password
 
 
 def generate_uuid():
@@ -16,3 +23,52 @@ def generate_uuid():
 
 class Base(DeclarativeBase):
     created_at = Column(DateTime, server_default=func.now())
+
+class EncryptedField:
+    def __init__(self, column_name: str):
+        self.column_name = column_name
+
+    def __get__(self, obj, objtype=None) -> str | None:
+        if obj is None:
+            return None
+        raw_value = getattr(obj, f"_{self.column_name}")
+        if raw_value is None:
+            return None
+        return decrypt_password(raw_value)
+
+    def __set__(self, obj, value: str) -> None:
+        if value is None or value == "":
+            return None
+        encrypted = encrypt_password(value)
+        setattr(obj, f"_{self.column_name}", encrypted)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID, primary_key=True, default=generate_uuid)
+    nickname = Column(String(50), nullable=False)
+    email = Column(String(256), nullable=False)
+    password = EncryptedField("password")
+
+    recention = relationship("Recention", back_populates="user")
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(UUID, primary_key=True, default=generate_uuid)
+    tmdb_id = Column(Integer, nullable=False)   
+    genres = Column(ARRAY(String(25)), nullable=False)
+
+    recention = relationship("Recention", back_populates="movie")
+
+class Recention(Base):
+    __tablename__ = "recentions"
+
+    id = Column(UUID, primary_key=True, default=generate_uuid)
+    rate = Column(Integer, nullable=False, default=0)
+    comment = Column(Text, nullable=True)
+    movie_status = Column(String(10), nullable=False)
+    
+    
+    user = relationship("User", back_populates="recention")
+    movie = relationship("Movie", back_populates="recention")
--- app/settings.py
+++ app/settings.py
@@ -1,6 +1,9 @@
+import hashlib
 import logging
 import sys
 
+from base64 import urlsafe_b64encode
+from cryptography.fernet import Fernet
 from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -15,7 +18,10 @@ class Settings(BaseSettings):
     DB_USER: str = Field("postgres")
     DB_PASS: str = Field("postgres")
 
+    ENCRYPT_SECRET_KEY: str
+
     logger: logging.Logger = logging.getLogger("uvicorn.info")
+    fernet: Fernet | None = Field(default=None, init=False)
 
     @field_validator("DB_NAME", mode="before")
     def set_db_name(cls, db, info):
@@ -25,9 +31,15 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file="conf/.env", env_file_encoding="utf-8")
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        key_bytes = hashlib.sha256(self.ENCRYPT_SECRET_KEY.encode("utf-8")).digest()  # 32 байта
+        key_b64 = urlsafe_b64encode(key_bytes).decode("utf-8")  # base64
+        self.fernet = Fernet(key_b64)
+
     @property
     def database_url(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-
 
 settings = Settings()
